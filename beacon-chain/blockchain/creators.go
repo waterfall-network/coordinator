074e93684bf8ea5511bbe79c9e04e2578a871e24
Vladimir Mezin <vm@waterfall.foundation>
2022-08-26 23:30:05 +0300

    fix: sort creators

--- beacon-chain/blockchain/creators.go
+++ beacon-chain/blockchain/creators.go
@@ -1,22 +1,30 @@
 package blockchain
 
 import (
+	"sort"
+	"sync"
+
 	types "github.com/prysmaticlabs/eth2-types"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/config/params"
 	"github.com/prysmaticlabs/prysm/time/slots"
-	"github.com/waterfall-foundation/gwat/common"
-	"sync"
+	gwatCommon "github.com/waterfall-foundation/gwat/common"
 )
 
 // This defines the current chain service's view of creators.
 type creatorsAssignment struct {
-	assignment map[types.Slot][]common.Address // creators' assignment by slot.
+	assignment map[types.Slot][]gwatCommon.Address // creators' assignment by slot.
 	lock       sync.RWMutex
 }
 
+type listValIx []types.ValidatorIndex
+
+func (v listValIx) Len() int           { return len(v) }
+func (v listValIx) Less(i, j int) bool { return v[i] < v[j] }
+func (v listValIx) Swap(i, j int)      { v[i], v[j] = v[j], v[i] }
+
 // GetCurrentCreators returns creators assignments for current slot.
-func (s *Service) GetCurrentCreators() ([]common.Address, error) {
+func (s *Service) GetCurrentCreators() ([]gwatCommon.Address, error) {
 	s.creators.lock.RLock()
 	defer s.creators.lock.RUnlock()
 	slot := s.HeadSlot() + 1
@@ -26,22 +34,29 @@ func (s *Service) GetCurrentCreators() ([]common.Address, error) {
 	}
 
 	// calculate creators assignments
-	creatorsAssig := make(map[types.Slot][]common.Address, params.BeaconConfig().SlotsPerEpoch)
+	creatorsAssig := make(map[types.Slot][]gwatCommon.Address, params.BeaconConfig().SlotsPerEpoch)
 	slotAssigIndexes := make(map[types.Slot][]types.ValidatorIndex, params.BeaconConfig().SlotsPerEpoch)
 	ctx := s.ctx
 	state := s.headState(ctx)
 	epoch := slots.ToEpoch(slot)
 	validatorIndexToCommittee, _, err := helpers.CommitteeAssignments(ctx, state, epoch)
 	if err != nil {
-		return []common.Address{}, err
+		return []gwatCommon.Address{}, err
 	}
 
-	for inx, val := range validatorIndexToCommittee {
+	vIxs := listValIx{}
+	for inx, _ := range validatorIndexToCommittee {
+		vIxs = append(vIxs, inx)
+	}
+	sort.Sort(vIxs)
+	for _, inx := range vIxs {
+		val := validatorIndexToCommittee[inx]
+
 		if slotAssigIndexes[val.AttesterSlot] == nil {
 			slotAssigIndexes[val.AttesterSlot] = []types.ValidatorIndex{}
 		}
 		if creatorsAssig[val.AttesterSlot] == nil {
-			creatorsAssig[val.AttesterSlot] = []common.Address{}
+			creatorsAssig[val.AttesterSlot] = []gwatCommon.Address{}
 		}
 		//check index in slot
 		isCreator := false
@@ -62,7 +77,7 @@ func (s *Service) GetCurrentCreators() ([]common.Address, error) {
 				continue
 			}
 			// Withdrawal address uses as gwat coinbase
-			address := common.BytesToAddress(validator.WithdrawalCredentials()[12:])
+			address := gwatCommon.BytesToAddress(validator.WithdrawalCredentials()[12:])
 			// skip already added
 			for _, addr := range creatorsAssig[val.AttesterSlot] {
 				if address == addr {
@@ -71,7 +86,7 @@ func (s *Service) GetCurrentCreators() ([]common.Address, error) {
 				}
 			}
 			if isCreator {
-				creatorsAssig[val.AttesterSlot] = append(creatorsAssig[val.AttesterSlot], address)
+				creatorsAssig[val.AttesterSlot] = gwatCommon.SortAddresses(append(creatorsAssig[val.AttesterSlot], address))
 			}
 		}
 	}
