4f9846e5cee0942d735d6d5617c80ee6e254962f
Vladimir Mezin <vm@waterfall.foundation>
2022-10-21 17:58:21 +0300

    fix: validation of candidates

--- beacon-chain/blockchain/process_block.go
+++ beacon-chain/blockchain/process_block.go
@@ -195,13 +195,15 @@ func (s *Service) onBlock(ctx context.Context, signed block.SignedBeaconBlock, b
 	}).Info("==== savePostStateInfo ====")
 
 	if !s.isSync(s.ctx) {
-		isValidCandidates, err := s.ValidateBlockCandidates(signed.Block())
-		if !isValidCandidates || err != nil {
-			log.WithError(err).WithField(
-				"slotCandidates", isValidCandidates,
-			).Warn("!!!!!! onBlock: validation of candidates failed")
-			return errBadSpineCandidates
-		}
+		log.WithError(errSyncIsRunning).Warn("!!!!!! onBlock: skip head update")
+		return errSyncIsRunning
+	}
+	isValidCandidates, err := s.ValidateBlockCandidates(signed.Block())
+	if !isValidCandidates || err != nil {
+		log.WithError(err).WithField(
+			"slotCandidates", isValidCandidates,
+		).Warn("!!!!!! onBlock: validation of candidates failed")
+		return errBadSpineCandidates
 	}
 
 	// If slasher is configured, forward the attestations in the block via
