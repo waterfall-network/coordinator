3ef287aea8ece5e0461dabe374814fd7ce8f3cc1
Vladimir Mezin <vm@waterfall.foundation>
2023-10-12 15:45:50 +0300

    fix: parallel sync

--- beacon-chain/db/kv/spines.go
+++ beacon-chain/db/kv/spines.go
@@ -2,22 +2,16 @@ package kv
 
 import (
 	"context"
-	"sync"
 
 	"gitlab.waterfall.network/waterfall/protocol/coordinator/proto/prysm/v1alpha1/wrapper"
 	bolt "go.etcd.io/bbolt"
 	"go.opencensus.io/trace"
 )
 
-var spinesMu sync.RWMutex
-
 // ReadSpines retrieval spines represented by plain bytes array.
 func (s *Store) ReadSpines(ctx context.Context, key [32]byte) (wrapper.Spines, error) {
 	ctx, span := trace.StartSpan(ctx, "BeaconDB.ReadSpines")
 	defer span.End()
-
-	spinesMu.RLock()
-	defer spinesMu.RUnlock()
 
 	var err error
 	var data wrapper.Spines
